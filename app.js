44214c3f13a096e49810b84caa812fe8e5039582
deHewett <42918243+deHewett@users.noreply.github.com>
2019-08-05 09:50:31 +1200

    routes

--- app.js
+++ app.js
@@ -2,124 +2,16 @@ var http = require("http");
 var express = require("express");
 var app = express();
 var fs = require('fs');
+var router = require('./Routes');
 const port = 8081;
 
- app.get('/', function(req, res){
-     res.writeHead(200, {'Content-Type': 'text/html'
+ 
+
+let handleRequest = (req, res) => {
+    res.writeHead(200, {
+        'Content-Type': 'text/html'
     });
-    fs.readFile('./Routes/index.html', null, function (error, data) {
-        if (error) {
-            res.writeHead(404);
-            res.write('Whoops! File not found!');
-        } else {
-            res.write(data);
-        }
-        res.end();
-    });
- })
-
- app.get('/login', function(req, res){
-    res.writeHead(200, {'Content-Type': 'text/html'
-   });
-   fs.readFile('./Routes/login.html', null, function (error, data) {
-       if (error) {
-           res.writeHead(404);
-           res.write('Whoops! File not found!');
-       } else {
-           res.write(data);
-       }
-       res.end();
-   });
-})
-
-app.get('/products', function(req, res){
-    res.writeHead(200, {'Content-Type': 'text/html'
-   });
-   fs.readFile('./Routes/products.html', null, function (error, data) {
-       if (error) {
-           res.writeHead(404);
-           res.write('Whoops! File not found!');
-       } else {
-           res.write(data);
-       }
-       res.end();
-   });
-})
-
-app.get('/products/:id', function(req, res){
-    const productId = req.params.id;
-    res.writeHead(200, {'Content-Type': 'text/html'
-   });
-   fs.readFile('./Routes/products.html', null, function (error, data) {
-       if (error) {
-           res.writeHead(404);
-           res.write('Whoops! File not found!');
-       } else {
-           res.write(data);
-       }
-       res.end();
-   });
-})
-
-app.get('/profile', function(req, res){
-    res.writeHead(200, {'Content-Type': 'text/html'
-   });
-   fs.readFile('./Routes/profile.html', null, function (error, data) {
-       if (error) {
-           res.writeHead(404);
-           res.write('Whoops! File not found!');
-       } else {
-           res.write(data);
-       }
-       res.end();
-   });
-})
-
-app.get('/staff', function(req, res){
-    res.writeHead(200, {'Content-Type': 'text/html'
-   });
-   fs.readFile('./Routes/staff.html', null, function (error, data) {
-       if (error) {
-           res.writeHead(404);
-           res.write('Whoops! File not found!');
-       } else {
-           res.write(data);
-       }
-       res.end();
-   });
-})
-
-app.get('/cart', function(req, res){
-    res.writeHead(200, {'Content-Type': 'text/html'
-   });
-   fs.readFile('./Routes/cart.html', null, function (error, data) {
-       if (error) {
-           res.writeHead(404);
-           res.write('Whoops! File not found!');
-       } else {
-           res.write(data);
-       }
-       res.end();
-   });
-})
-
-app.get('/contact', function(req, res){
-    res.writeHead(200, {'Content-Type': 'text/html'
-   });
-   fs.readFile('./Routes/contact.html', null, function (error, data) {
-       if (error) {
-           res.writeHead(404);
-           res.write('Whoops! File not found!');
-       } else {
-           res.write(data);
-       }
-       res.end();
-   });
-})
-
- var server = app.listen(port, function(){
-     var host = server.address().address
-     var port = server.address().port
-
-     console.log("Example app listening at http://%s:%s", host, port)
- })
+ 
+};
+ 
+http.createServer(router.handleRequest).listen(port);
