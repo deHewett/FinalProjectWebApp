31bd1023c4b74bedcc53a0a487fd8484ae4f7de3
deHewett <42918243+deHewett@users.noreply.github.com>
2019-08-05 09:32:20 +1200

    babel etc

--- app.js
+++ app.js
@@ -18,6 +18,105 @@ const port = 8081;
     });
  })
 
+ app.get('/login', function(req, res){
+    res.writeHead(200, {'Content-Type': 'text/html'
+   });
+   fs.readFile('./Routes/login.html', null, function (error, data) {
+       if (error) {
+           res.writeHead(404);
+           res.write('Whoops! File not found!');
+       } else {
+           res.write(data);
+       }
+       res.end();
+   });
+})
+
+app.get('/products', function(req, res){
+    res.writeHead(200, {'Content-Type': 'text/html'
+   });
+   fs.readFile('./Routes/products.html', null, function (error, data) {
+       if (error) {
+           res.writeHead(404);
+           res.write('Whoops! File not found!');
+       } else {
+           res.write(data);
+       }
+       res.end();
+   });
+})
+
+app.get('/products/:id', function(req, res){
+    const productId = req.params.id;
+    res.writeHead(200, {'Content-Type': 'text/html'
+   });
+   fs.readFile('./Routes/products.html', null, function (error, data) {
+       if (error) {
+           res.writeHead(404);
+           res.write('Whoops! File not found!');
+       } else {
+           res.write(data);
+       }
+       res.end();
+   });
+})
+
+app.get('/profile', function(req, res){
+    res.writeHead(200, {'Content-Type': 'text/html'
+   });
+   fs.readFile('./Routes/profile.html', null, function (error, data) {
+       if (error) {
+           res.writeHead(404);
+           res.write('Whoops! File not found!');
+       } else {
+           res.write(data);
+       }
+       res.end();
+   });
+})
+
+app.get('/staff', function(req, res){
+    res.writeHead(200, {'Content-Type': 'text/html'
+   });
+   fs.readFile('./Routes/staff.html', null, function (error, data) {
+       if (error) {
+           res.writeHead(404);
+           res.write('Whoops! File not found!');
+       } else {
+           res.write(data);
+       }
+       res.end();
+   });
+})
+
+app.get('/cart', function(req, res){
+    res.writeHead(200, {'Content-Type': 'text/html'
+   });
+   fs.readFile('./Routes/cart.html', null, function (error, data) {
+       if (error) {
+           res.writeHead(404);
+           res.write('Whoops! File not found!');
+       } else {
+           res.write(data);
+       }
+       res.end();
+   });
+})
+
+app.get('/contact', function(req, res){
+    res.writeHead(200, {'Content-Type': 'text/html'
+   });
+   fs.readFile('./Routes/contact.html', null, function (error, data) {
+       if (error) {
+           res.writeHead(404);
+           res.write('Whoops! File not found!');
+       } else {
+           res.write(data);
+       }
+       res.end();
+   });
+})
+
  var server = app.listen(port, function(){
      var host = server.address().address
      var port = server.address().port
