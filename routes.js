0daeed629968d1f8b80050ddb10cf8bdc804e0f0
deHewett <42918243+deHewett@users.noreply.github.com>
2019-08-13 11:56:49 +1200

    edits

--- routes.js
+++ routes.js
@@ -10,10 +10,10 @@ const passport = require("passport");
 
 const routes = (app) => {
     app.get('/', function(req,res) {
-        res.sendFile(path.join(__dirname + "/Routes/index.html"));
+        res.sendFile(path.join(__dirname + "/Views/index.html"));
     });
     app.get('/login', function(req,res) {
-        res.sendFile(path.join(__dirname + '/Routes/login.html'));
+        res.sendFile(path.join(__dirname + '/Views/login.html'));
     });
     app.get('/profile', function(req,res) {
         res.sendFile(path.join(__dirname + '/Views/profile.html'));
